fe33f070c1033c190f885b2f973424a657fbb193
unknown <sergey@anexspb.ru>
2019-09-16 19:59:39 +0300

    Р4

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,35 +4,19 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow(x, n) {
-    let result = x;
-
-<<<<<<< HEAD
-if (n < 0){
-    alert('Введите натуральное число!');
-    n = prompt('Ведите n', '');
-}
+   
+let x = +prompt('Введите x', '');
+let n = +prompt('Введите n', '');
 
 function pow(x, n){
-    return x**n;
+
+let result = x**Math.round(n);
+
+return result;
 }
 
-alert(pow(x, n));
-=======
-    for (let i = 1; i < n; i++) {
-        result *= x;
-    }
-
-    return result;
-}
-
-let x = prompt("x?", '');
-let n = prompt("n?", '');
-
-if (n < 1) {
-    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+if (n <= 0){
+    alert('Введите целое, натуральное число');
 } else {
-    alert( pow(x, n) );
+    alert(pow(x, n));
 }
->>>>>>> 1188bd279be4643be093d826dcb5c5df6638a6b0
-
